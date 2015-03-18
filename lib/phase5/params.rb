e646445417a25dab023fbe60778e3af353788133
Tripp Davis <trippdavis3@gmail.com>
2015-03-18 10:01:24 -0700

    params completed

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -10,9 +10,13 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+      @params = route_params
+      parse_www_encoded_form(req.query_string) if req.query_string
+      parse_www_encoded_form(req.body) if req.body
     end
 
     def [](key)
+      @params[key]
     end
 
     def to_s
@@ -28,11 +32,28 @@ module Phase5
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      decoded = URI::decode_www_form(www_encoded_form)
+      decoded.each do |decode|
+        keys = parse_key(decode[0])
+        value = decode[1]
+        current = @params
+        keys.each_with_index do |key, idx|
+          if idx == (keys.length - 1)
+            current[key] = value
+          else
+            unless current[key].is_a?(Hash)
+              current[key] = {}
+            end
+            current = current[key]
+          end
+        end
+      end
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      key.gsub(/[\[\]]/," ").split(" ")
     end
   end
 end
